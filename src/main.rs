44dd829ec28aea51d0272eec9357c88123c745a9
Ted Pilcher <ted.pilcher@moodys.com>
2025-03-25 17:00:15 +0000

    Update main

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,21 @@ use assets::Asset;
 use crossterm::event::{self, Event, KeyCode, KeyEventKind};
 use internal_terminal::{Command, InternalTerminalWidget};
 use ratatui::Terminal;
-use ratatui::layout::Offset;
+use ratatui::layout::Layout;
 use ratatui::prelude::Backend;
-use ratatui::prelude::Constraint::{Fill, Length, Min};
+use ratatui::prelude::Constraint::{Length, Min};
 use ratatui::text::Text;
-use ratatui::widgets::{Block, StatefulWidget};
-use ratatui::{Frame, layout::Layout};
+use ratatui::widgets::Block;
 use state::{Interval, State};
 use yahoo_finance_api::YahooConnector;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let mut terminal = ratatui::init();
     let mut state = State::new();
+
+    let mut finance_provider =
+        YahooConnector::new().expect("Should be able to create YahooConnectoir");
 
     loop {
         draw(&mut terminal, &mut state);
@@ -26,12 +29,16 @@ fn main() -> Result<()> {
             match command {
                 Command::Exit => break,
                 Command::AddAsset(ticker) => {
-                    let asset = Asset::new(ticker.clone());
-                    state.assets.insert(ticker, asset);
+                    if let Ok(asset) =
+                        Asset::try_new(ticker.clone(), &mut finance_provider, &state.interval).await
+                    {
+                        state.assets.insert(ticker, asset);
+                    }
                 }
                 Command::RemoveAsset(ticker) => {
                     state.assets.remove_entry(&ticker);
                 }
+                // TODO: Will need to refetch data
                 Command::ChangeInterval(interval) => {
                     if let Ok(interval) = Interval::try_from(interval) {
                         state.interval = interval
@@ -46,27 +53,30 @@ fn main() -> Result<()> {
 }
 
 fn draw(terminal: &mut Terminal<impl Backend>, state: &mut State) {
-    let internal_terminal_text = Text::raw(state.internal_terminal.get_text());
+    let _internal_terminal_text = Text::raw(state.internal_terminal.get_text());
+
     let stocks_text = Text::raw(
         state
             .assets
-            .keys()
-            .map(|k| k.as_str())
-            .collect::<Vec<&str>>()
+            .values()
+            .map(|asset| [asset.get_ticker(), &asset.get_price().to_string()].join(", "))
+            .collect::<Vec<String>>()
             .join(", "),
     );
-    let interval_text = Text::raw(String::from(state.interval.clone()));
+
+    let interval_text = Text::raw(String::from(&state.interval));
 
     let vertical = Layout::vertical([Length(1), Min(0), Length(5), Length(5)]);
 
     let _ = terminal.draw(|frame| {
         let [title_area, heatmap_area, interval_area, terminal_area] = vertical.areas(frame.area());
-        // frame.render_widget(internal_terminal_text, frame.area());
-        // frame.render_widget(stocks_text, frame.area());
-        // frame.render_widget(interval_text, frame.area());
         frame.render_widget(Block::bordered().title("Asset Heatmap"), title_area);
         let internal_terminal = &mut state.internal_terminal;
         frame.render_stateful_widget(InternalTerminalWidget {}, terminal_area, internal_terminal);
+        frame.render_widget(stocks_text, heatmap_area);
+
+        // Render each INTERVAL and highlight the one currently selected
+        frame.render_widget(interval_text, interval_area);
     });
 }
 
