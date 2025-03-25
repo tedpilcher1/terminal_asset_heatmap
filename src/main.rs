fa99018e15dbcf5210ae5fbcea8f869f7d60ff88
Ted Pilcher <tedspilcher@gmail.com>
2025-03-25 21:33:36 +0000

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,38 @@
 pub mod assets;
 pub mod internal_terminal;
+pub mod interval;
 pub mod state;
 
 use anyhow::Result;
 use assets::Asset;
 use crossterm::event::{self, Event, KeyCode, KeyEventKind};
 use internal_terminal::{Command, InternalTerminalWidget};
+use interval::{Interval, IntervalWidget};
 use ratatui::Terminal;
-use ratatui::layout::Layout;
+use ratatui::layout::{Layout, Margin};
 use ratatui::prelude::Backend;
 use ratatui::prelude::Constraint::{Length, Min};
-use ratatui::text::Text;
 use ratatui::widgets::Block;
-use state::{Interval, State};
+use state::{AssetHeatmapWidget, State};
 use yahoo_finance_api::YahooConnector;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut terminal = ratatui::init();
-    let mut state = State::new();
-
     let mut finance_provider =
-        YahooConnector::new().expect("Should be able to create YahooConnectoir");
+        YahooConnector::new().expect("Should be able to create YahooConnector");
+    let mut terminal = ratatui::init();
+    let mut state = State::new_with_assets(&mut finance_provider).await;
 
     loop {
         draw(&mut terminal, &mut state);
         if let Some(command) = handle_events(&mut state)? {
             match command {
                 Command::Exit => break,
+                Command::Update => {
+                    state
+                        .update_interval(state.interval.clone(), &mut finance_provider)
+                        .await
+                }
                 Command::AddAsset(ticker) => {
                     if let Ok(asset) =
                         Asset::try_new(ticker.clone(), &mut finance_provider, &state.interval).await
@@ -38,10 +43,9 @@ async fn main() -> Result<()> {
                 Command::RemoveAsset(ticker) => {
                     state.assets.remove_entry(&ticker);
                 }
-                // TODO: Will need to refetch data
                 Command::ChangeInterval(interval) => {
                     if let Ok(interval) = Interval::try_from(interval) {
-                        state.interval = interval
+                        state.update_interval(interval, &mut finance_provider).await
                     }
                 }
             }
@@ -53,30 +57,18 @@ async fn main() -> Result<()> {
 }
 
 fn draw(terminal: &mut Terminal<impl Backend>, state: &mut State) {
-    let _internal_terminal_text = Text::raw(state.internal_terminal.get_text());
-
-    let stocks_text = Text::raw(
-        state
-            .assets
-            .values()
-            .map(|asset| [asset.get_ticker(), &asset.get_price().to_string()].join(", "))
-            .collect::<Vec<String>>()
-            .join(", "),
-    );
-
-    let interval_text = Text::raw(String::from(&state.interval));
-
-    let vertical = Layout::vertical([Length(1), Min(0), Length(5), Length(5)]);
-
+    let vertical = Layout::vertical([Length(1), Min(0), Length(3), Length(3)]);
     let _ = terminal.draw(|frame| {
         let [title_area, heatmap_area, interval_area, terminal_area] = vertical.areas(frame.area());
-        frame.render_widget(Block::bordered().title("Asset Heatmap"), title_area);
+        frame.render_widget(Block::bordered().title(" Asset Heatmap "), title_area);
         let internal_terminal = &mut state.internal_terminal;
         frame.render_stateful_widget(InternalTerminalWidget {}, terminal_area, internal_terminal);
-        frame.render_widget(stocks_text, heatmap_area);
-
-        // Render each INTERVAL and highlight the one currently selected
-        frame.render_widget(interval_text, interval_area);
+        frame.render_stateful_widget(IntervalWidget {}, interval_area, &mut state.interval);
+        frame.render_stateful_widget(
+            AssetHeatmapWidget {},
+            heatmap_area.inner(Margin::new(1, 1)),
+            &mut state.assets,
+        );
     });
 }
 
